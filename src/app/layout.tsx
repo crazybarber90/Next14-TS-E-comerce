cd411b7932c4f90be976ae8408b6394738d57646
Nikola <pepy90aa@gmail.com>
2024-05-27 23:37:46 +0200

    working on header/search and navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,28 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Lama Dev E-Commerce Application",
-  description: "A complete e-commerce application with Next.js and Wix",
-};
+  title: 'Lama Dev E-Commerce Application',
+  description: 'A complete e-commerce application with Next.js and Wix',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
-  );
+  )
 }
---
+++ src/components/NavIcons.tsx
@@ -0,0 +1,8 @@
+'use client'
+import React from 'react'
+
+const NavIcons = () => {
+  return <div>NavIcons</div>
+}
+
+export default NavIcons
